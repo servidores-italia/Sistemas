81efd9e78d68128b6063937d4605a85be6d1a8ea
servidores-italia <L16180921@slp.tecnm.mx>
2020-12-07 15:08:58 -0600

    Base de datos

---
+++ database/seeders/LibroSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LibroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('libro')->insert([
+        	'nombre'=> 'Estructura de Datos',
+        	'autor'=> 'Luis Joyanes',
+        	
+
+        ]);
+
+        DB::table('libro')->insert([
+        	'nombre'=> 'Programacion Orientada a Objetos',
+        	'autor'=> 'Luis Joyanes',
+        	
+
+        ]);
+
+    }
+}
